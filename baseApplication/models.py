ad94838baf4f0341bff2fb0ee0d728a680a3d77e
volpertinger <merzlovnik@mail.ru>
2022-04-13 00:22:05 +0300

    Profile model

--- baseApplication/models.py
+++ baseApplication/models.py
@@ -1,26 +1,29 @@
 from django.db import models
-from django.contrib import auth
+from django.contrib.auth.models import User
 
 
 class Reputation(models.Model):
     value = models.IntegerField
 
 
+class Profile(User):
+    profile_image = models.ImageField()
+
+
 class Question(models.Model):
     title = models.CharField(max_length=256)
     text = models.TextField(max_length=8192)
-    reputation = models.ForeignKey(Reputation, models.CASCADE)
-    # author'
+    reputation = models.ForeignKey(Reputation, on_delete=models.CASCADE)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
 
 class Answer(models.Model):
     text = models.TextField(max_length=8192)
     reputation = models.ForeignKey(Reputation, on_delete=models.CASCADE)
     isCorrect = models.BooleanField()
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
 
 class Tag(models.Model):
     tag = models.CharField(max_length=32)
     questions = models.ManyToManyField(Question)
-
-# class profile
