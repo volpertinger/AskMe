6fdee93d20390158a90f2a0e2b592ecbeb9ee510
volpertinger <merzlovnik@mail.ru>
2022-04-13 00:17:29 +0300

    DB models

--- baseApplication/models.py
+++ baseApplication/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib import auth
 
-# Create your models here.
+
+class Reputation(models.Model):
+    value = models.IntegerField
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=256)
+    text = models.TextField(max_length=8192)
+    reputation = models.ForeignKey(Reputation, models.CASCADE)
+    # author'
+
+
+class Answer(models.Model):
+    text = models.TextField(max_length=8192)
+    reputation = models.ForeignKey(Reputation, on_delete=models.CASCADE)
+    isCorrect = models.BooleanField()
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=32)
+    questions = models.ManyToManyField(Question)
+
+# class profile
