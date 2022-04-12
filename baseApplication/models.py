9f7df2d353a2665ad74069e02faf8c9f558bc7ea
volpertinger <merzlovnik@mail.ru>
2022-04-13 01:21:35 +0300

    simple models update

--- baseApplication/models.py
+++ baseApplication/models.py
@@ -5,9 +5,15 @@ from django.contrib.auth.models import User
 class Reputation(models.Model):
     value = models.IntegerField
 
+    def __str__(self):
+        return str(self.value)
+
 
 class Profile(User):
     profile_image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.username)
 
 
 class Question(models.Model):
@@ -16,6 +22,14 @@ class Question(models.Model):
     reputation = models.ForeignKey(Reputation, on_delete=models.CASCADE)
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
+    # objects = QuestionManager
+
+    def getHighReputation(self, number):
+        return self.objects.all().order_by("reputation")[:number]
+
+    def __str__(self):
+        return self.title
+
 
 class Answer(models.Model):
     text = models.TextField(max_length=8192)
@@ -23,7 +37,13 @@ class Answer(models.Model):
     isCorrect = models.BooleanField()
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.author)
+
 
 class Tag(models.Model):
     tag = models.CharField(max_length=32)
     questions = models.ManyToManyField(Question, blank=True)
+
+    def __str__(self):
+        return self.tag
